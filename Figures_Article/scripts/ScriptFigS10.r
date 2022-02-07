d8ba62aef829acc710fd6c38dd8b23f968f137d6
LacroixLaurent <laurent.lacroix@inserm.fr>
2022-02-07 14:51:15 +0100

    change  other to rest of the genome

--- Figures_Article/scripts/ScriptFigS10.r
+++ Figures_Article/scripts/ScriptFigS10.r
@@ -11,17 +11,18 @@ pathdata <- "/Users/ll/work/RStudioProjects/NanoForkSpeed/Figures_Article/data/"
 pval <- read_tsv(paste0(pathdata,"pval_table.tsv.gz"))
 
 ### CEN
-toplot <- read_tsv(paste0(pathdata,"FigureS10A_data.tsv.gz"))
-toplot$feat_name <- factor(toplot$feat_name,levels=c(paste0("CEN",1:16),"other"))
+toplot <- read_tsv(paste0(pathdata,"FigureS10A_data.tsv.gz")) %>% mutate(feat_name=case_when(feat_name=="other"~"Rest of the\nGenome",T~feat_name))
+toplot$feat_name <- factor(toplot$feat_name,levels=c(paste0("CEN",1:16),"Rest of the\nGenome"))
 toplot2 <- toplot %>% group_by(feat_name) %>% summarise(n=n(),m=round(mean(mea)),sd=sd(mea,na.rm=T)) %>% ungroup
-topval <- left_join(toplot2 %>% select(feat_name),pval %>% filter(Figure=="S10A") %>% select(feat_name=type,signif,pval_adj))
+topval <- left_join(toplot2 %>% select(feat_name),pval %>% filter(Figure=="S10A") %>% select(feat_name=type,signif,pval_adj,signif1,signif2))
 
 fs10a <- ggplot(toplot,aes(x=feat_name,y=mea))+
 	geom_point(data=toplot2,aes(x=feat_name,y=m),col="red",shape=95,size=10,show.legend=F)+
 	geom_errorbar(data=toplot2,aes(x=feat_name,y=m,ymin=m-sd,ymax=m+sd),col="red",width=0.2)+
-	stat_dots(col="black",shape=16,side="both",dotsize=1.5,alpha=0.6)+
+	stat_dots(col="black",shape=16,side="both",dotsize=2,alpha=0.6)+
 	geom_text(data=toplot2,aes(x=feat_name,y=3900,label=m),col="red",size=3) +
-	geom_text(data=topval,aes(x=feat_name,y=3600,label=signif),col="black",size=5) +
+	geom_text(data=topval,aes(x=feat_name,y=3550,label=signif1),col="black",size=5) +
+	geom_text(data=topval,aes(x=feat_name,y=3650,label=signif2),col="black",size=3) +
 	geom_text(data=toplot2,aes(x=feat_name,y=0,label=n),fontface="italic",size=3) +
 	coord_cartesian(ylim=c(0,4000))+
 	scale_colour_manual("",values=mypal[c(3:18,1)])+
@@ -32,17 +33,18 @@ fs10a <- ggplot(toplot,aes(x=feat_name,y=mea))+
 
 
 ### TEL
-toplot <- read_tsv(paste0(pathdata,"FigureS10B_data.tsv.gz"))
+toplot <- read_tsv(paste0(pathdata,"FigureS10B_data.tsv.gz"))%>% mutate(feat_name=case_when(feat_name=="other"~"Rest of the\nGenome",T~feat_name))
 toplot$feat_name <- factor(toplot$feat_name,levels=unique(toplot$feat_name)[c(1:15,17,16)])
 toplot2 <- toplot %>% group_by(feat_name) %>% summarise(n=n(),m=round(mean(mea)),sd=sd(mea,na.rm=T)) %>% ungroup
-topval <- left_join(toplot2 %>% select(feat_name),pval %>% filter(Figure=="S10B") %>% select(feat_name=type,signif,pval_adj))
+topval <- left_join(toplot2 %>% select(feat_name),pval %>% filter(Figure=="S10B") %>% select(feat_name=type,signif,pval_adj,signif1,signif2))
 
 fs10b <- ggplot(toplot,aes(x=feat_name,y=mea))+
 	geom_point(data=toplot2,aes(x=feat_name,y=m),col="red",shape=95,size=10,show.legend=F)+
 	geom_errorbar(data=toplot2,aes(x=feat_name,y=m,ymin=m-sd,ymax=m+sd),col="red",width=0.2)+
-	stat_dots(col="black",shape=16,side="both",dotsize=1.5,alpha=0.6)+
+	stat_dots(col="black",shape=16,side="both",dotsize=2,alpha=0.6)+
 	geom_text(data=toplot2,aes(x=feat_name,y=3900,label=m),col="red",size=3) +
-	geom_text(data=topval,aes(x=feat_name,y=3600,label=signif),col="black",size=5) +
+	geom_text(data=topval,aes(x=feat_name,y=3550,label=signif1),col="black",size=5) +
+	geom_text(data=topval,aes(x=feat_name,y=3650,label=signif2),col="black",size=3) +
 	geom_text(data=toplot2,aes(x=feat_name,y=0,label=n),fontface="italic",size=3) +
 	coord_cartesian(ylim=c(0,4000))+
 	scale_colour_manual("",values=mypal[c(3:18,1)])+

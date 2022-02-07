1ba7dabbb46e48cf5b6b9de07657ca5994c95738
LacroixLaurent <laurent.lacroix@inserm.fr>
2022-02-07 13:55:41 +0100

    adjust dot size and RT limits

--- Figures_Article/scripts/ScriptFig6ABCD.r
+++ Figures_Article/scripts/ScriptFig6ABCD.r
@@ -16,7 +16,7 @@ toplot$feat <- factor(toplot$feat,levels=c("centromere","telomere","rDNA","tRNA"
 
 toplot <- toplot %>% mutate(feat=fct_recode(feat,"Centromeres"="centromere","Telomeres"="telomere","rDNA"="rDNA","tRNA genes"="tRNA","Rest of the\n genome"="other"))
 toplot2 <- toplot %>% group_by(feat) %>% summarise(m=round(mean(mea)),sd=sd(mea,na.rm=T),n=n()) %>% ungroup
-topval <- left_join(toplot2 %>% select(feat),pval %>% filter(Figure=="6A") %>% select(feat=type,signif,pval_adj) %>% mutate(feat=fct_recode(feat,"Centromeres"="centromere","Telomeres"="telomere","rDNA"="rDNA","tRNA genes"="tRNA")))
+topval <- left_join(toplot2 %>% select(feat),pval %>% filter(Figure=="6A") %>% select(feat=type,signif,pval_adj,signif1,signif2) %>% mutate(feat=fct_recode(feat,"Centromeres"="centromere","Telomeres"="telomere","rDNA"="rDNA","tRNA genes"="tRNA")))
 
 
 f6a <- ggplot(toplot,aes(x=feat,y=mea))+
@@ -25,7 +25,8 @@ f6a <- ggplot(toplot,aes(x=feat,y=mea))+
 	stat_dots(col="black",shape=16,side="both",dotsize=1.5,alpha=0.6)+
 	geom_text(data=toplot2,aes(x=feat,y=0,label=n),col="black",fontface="italic",size=3) +
 	geom_text(data=toplot2,aes(x=feat,y=3400,label=m),col="red",size=3) +
-	geom_text(data=topval,aes(x=feat,y=3100,label=signif),size=5) +
+	geom_text(data=topval,aes(x=feat,y=3100,label=signif1),size=5) +
+	geom_text(data=topval,aes(x=feat,y=3150,label=signif2),size=3) +
 	scale_colour_manual("Features",values=mypal[c(3,5,7,9,1)])+
 	coord_cartesian(ylim=c(0,3500))+
 	theme(axis.text.x = element_text(angle=45,hjust=1),legend.position = "none",plot.tag=element_text(face="bold"))+
@@ -41,7 +42,7 @@ toplot2 <- toplot %>% group_by(type) %>% summarise(m=round(mean(mea)),sd=sd(mea,
 f6b <- ggplot(toplot,aes(x=type,y=mea))+
 	geom_point(data=toplot2,aes(x=type,y=m),col="red",shape=95,size=10,show.legend=F)+
 	geom_errorbar(data=toplot2,aes(x=type,y=m,ymin=m-sd,ymax=m+sd),col="red",width=0.2)+
-	stat_dots(col="black",shape=16,side="both",dotsize=1.5,alpha=0.6)+
+	stat_dots(col="black",shape=16,side="both",dotsize=1.8,alpha=0.6)+
 	geom_text(data=toplot2,aes(x=type,y=3400,label=m),col="red",size=3) +
 	annotate(geom="text",x=1.5,y=3100,label="*",col="black",size=5) +
 	geom_text(data=toplot2,aes(x=type,y=0,label=n),col="black",fontface="italic",size=3) +
@@ -60,10 +61,10 @@ toplot2 <- toplot %>% group_by(type) %>% summarise(m=round(mean(mea)),sd=sd(mea,
 f6c <- ggplot(toplot,aes(x=type,y=mea))+
 	geom_point(data=toplot2,aes(x=type,y=m),col="red",shape=95,size=10,show.legend=F)+
 	geom_errorbar(data=toplot2,aes(x=type,y=m,ymin=m-sd,ymax=m+sd),col="red",width=0.2)+
-	stat_dots(col="black",shape=16,side="both",dotsize=1.5,alpha=0.6)+
+	stat_dots(col="black",shape=16,side="both",dotsize=2.7,alpha=0.6)+
 	geom_text(data=toplot2,aes(x=type,y=3400,label=m),col="red",size=3) +
 	geom_text(data=toplot2,aes(x=type,y=0,label=n),col="black",fontface="italic",size=3) +
-	annotate(geom="text",x=1.5,y=3100,label="-",col="black",size=5) +
+	annotate(geom="text",x=1.5,y=3150,label="n.s.",col="black",size=3) +
 	scale_colour_manual("",values=mypal[c(1,7)])+
 	coord_cartesian(ylim=c(0,3500))+
 	theme(axis.text.x = element_text(angle=45,hjust=1),legend.position = "none",plot.tag=element_text(face="bold"))+
@@ -77,10 +78,10 @@ toplot2 <- read_tsv(paste0(pathdata,"Figure6D_data.tsv.gz"))
 library(ggpubr)
 
 toplot_lim <- toplot2 %>% filter(RTsc3>0.95 & RTsc3<1.95)
-f6d <- ggplot(toplot2,aes(x=RTsc3,y=speed))+
+f6d <- ggplot(toplot_lim,aes(x=RTsc3,y=speed))+
 	geom_hex()+
 	geom_smooth(data=toplot_lim,aes(x=RTsc3,y=speed))+
-	stat_cor(data=toplot_lim,aes(x=RTsc3,y=speed),label.y = 4000,method="spearman",cor.coef.name="rho",col="grey20",fontface="italic",size=4)+
+	stat_cor(data=toplot_lim,aes(x=RTsc3,y=speed),label.y = 5000,method="spearman",cor.coef.name="rho",col="grey20",fontface="italic",size=4)+
 	scale_fill_continuous(type = "viridis",direction=-1,option="magma")+
 	ylim(c(0,5000))+
 	ylab("Speed (bp/min)")+

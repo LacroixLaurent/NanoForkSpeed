a5e7cab482313d439c3d9197e2f4ef27565babba
LacroixLaurent <laurent.lacroix@inserm.fr>
2022-03-31 12:12:43 +0200

    change page and add dplyr:: for slice

--- Figures_Article/scripts/ScriptFigS11.r
+++ Figures_Article/scripts/ScriptFigS11.r
@@ -10,8 +10,8 @@ library(ggprism)
 mypal <- paletteer::paletteer_d("ggthemes::Classic_20")
 `%+%` <- paste0
 
-path_figures <- "/Users/ll/work/RStudioProjects/NanoForkSpeed/Figures_Article/figures/"
-pathdata <- "/Users/ll/work/RStudioProjects/NanoForkSpeed/Figures_Article/data/"
+path_figures <- "./Figures_Article/figures/"
+pathdata <- "./Figures_Article/data/"
 
 seqinf <- readRDS(paste0(pathdata,"seqinfS288CrDNA.rds"))
 
@@ -19,8 +19,8 @@ ini2plot <- read_tsv(paste0(pathdata,"FigureS11_data.tsv.gz"))
 
 ARS <- import(paste0(pathdata,"ARS_newman.bed"))
 rDNA <- import(paste0(pathdata,"sc3_RRNA.bed"))
-ars <- as_tibble(ARS) %>% rename(chrom=seqnames)
-rdna <- as_tibble(rDNA) %>% rename(chrom=seqnames)
+ars <- as_tibble(ARS) %>% mutate(chrom=seqnames)
+rdna <- as_tibble(rDNA) %>% mutate(chrom=seqnames)
 
 geno_leg <- c("Ini. segment","ORI","RFB","rDNA")
 geno_pal <- mypal[c(2,13,5,9)]
@@ -29,8 +29,8 @@ names(geno_pal) <- geno_leg
 p1 <- ggplot(ini2plot)+
 	geom_segment(aes(x=x0,xend=x1,y=width,yend=width,col="Ini. segment"),alpha=0.6)+
 	geom_segment(data=ars %>% filter(chrom=="chrXII"),aes(x=start,xend=end,y=0,yend=40000,col="ORI"),size=0.5)+
-	geom_rect(data=rdna %>% slice(1),aes(xmin=start,xmax=end,ymin=200,ymax=39800,col="RFB"),size=0.5)+
-	geom_rect(data=rdna %>% slice(2:5),aes(xmin=start,xmax=end,ymin=-500,ymax=-1000,fill="rDNA"),size=1,col=NA)+
+	geom_rect(data=rdna %>% dplyr::slice(1),aes(xmin=start,xmax=end,ymin=200,ymax=39800,col="RFB"),size=0.5)+
+	geom_rect(data=rdna %>% dplyr::slice(2:5),aes(xmin=start,xmax=end,ymin=-500,ymax=-1000,fill="rDNA"),size=1,col=NA)+
 	xlab("chrXII (kb)")+
 	ylab("Width (bp)")+
 	scale_x_continuous(guide = "prism_minor",

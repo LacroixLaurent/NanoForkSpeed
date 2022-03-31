2dbbaa969e725a7739504352cdc2387eaf64b3cc
LacroixLaurent <laurent.lacroix@inserm.fr>
2022-03-31 10:58:40 +0200

    change blue

--- Figures_Article/scripts/ScriptFigS2AB.r
+++ Figures_Article/scripts/ScriptFigS2AB.r
@@ -17,7 +17,7 @@ pdf(file = paste0(path_figures,"FigureS2A.pdf"), width = 8, height = 4)
 ggplot(data_gc)+
 geom_line(aes(x=Time,y=log(OD600,base=10),group=Strain,color=Strain),size=0.5)+
 geom_point(aes(x=Time,y=log(OD600,base=10),group=Strain,color=Strain),size=1)+
-scale_color_manual(values=c("chartreuse4","navy","firebrick1"))+
+scale_color_manual(values=c("chartreuse4","blue","firebrick1"))+
 xlab("Time (minute)")+
 ylab("LOG10(OD600)")+
 theme_bw()
@@ -34,7 +34,7 @@ data_dt%>%group_by(Strain)%>%
 summarise(med_dt=median(Doubling_time),max_dt=max(Doubling_time),min_dt=min(Doubling_time))->stat_dt
 
 # Plot doubling time stats saved as a pdf file
-pdf(file = paste0(path_figures,"FigureS2A.pdf"), width = 8, height = 4)
+pdf(file = paste0(path_figures,"FigureS2B.pdf"), width = 8, height = 4)
 ggplot(data_dt)+
 geom_point(aes(x=Strain,y=Doubling_time,color=Strain),size=1.5)+
 ylim(0,150)+
@@ -42,7 +42,7 @@ ylab("Doubling time (minute)")+
 xlab("Strain")+
 geom_errorbar(data=stat_dt,aes(ymin=min_dt,ymax=max_dt,x=Strain),width = 0.15,alpha=0.9)+
 geom_point(data=stat_dt,aes(x=Strain,y=med_dt,group=Strain),shape=3,size=2,alpha=0.9)+
-scale_color_manual(values=c( "chartreuse4","navy","firebrick1"))+
+scale_color_manual(values=c( "chartreuse4","blue","firebrick1"))+
 theme_bw()
 dev.off()
 
